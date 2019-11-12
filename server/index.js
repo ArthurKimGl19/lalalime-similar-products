d3f79ff92c89e0be1eb1b99aed465796c546112f
ArthurKimGl19 <cyetmwi@gmail.com>
2019-11-11 17:31:32 -0800

    revert to previous commit

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ var nr = require('newrelic');
 const express = require('express')
 const app = express()
 const port = 3003
-
 const bodyParser = require('body-parser');
 const path = require('path');
 const router = require('./router.js');
@@ -13,8 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use('/api', router);
 
-app.use('loaderio-0d8974336037cfc2861e7d9cd78ef7c2', (req, res)=> {
-	res.send('loaderio-0d8974336037cfc2861e7d9cd78ef7c2/');
+app.use('/loaderio-27c8f340067c8ff5623b22cbbf4034ea/', (req, res)=> {
+	res.send('loaderio-27c8f340067c8ff5623b22cbbf4034ea/');
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
